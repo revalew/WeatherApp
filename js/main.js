ee2bef43e97518cf32e65b29447ae6fc9ee76a00
revalew <115182776+revalew@users.noreply.github.com>
2023-02-28 00:05:06 +0100

    added RegExp to determine the correct conditions icon and finished the project

--- js/main.js
+++ js/main.js
@@ -21,13 +21,14 @@ const getGeoCode = () => {
 	axios
 		.get(URL)
 		.then(res => {
-			getWeather(res.data[0].lat, res.data[0].lon)
-			// console.log(res);
-			// console.log(res.data[0].lat, res.data[0].lon);
+			// I decided to assign the city name here (getGeoCode()) because, for example, Tokyo works fine here BUT, when passed through the 2nd API (getWeather()), it returns the city name as "Japan". Like seriously, why!?
 			cityName.textContent = res.data[0].name
+			getWeather(res.data[0].lat, res.data[0].lon)
+			clearInput()
 		})
 		.catch(err => {
 			console.error(err)
+			warning.textContent = 'Wpisz poprawną nazwę miasta!'
 		})
 }
 
@@ -39,7 +40,10 @@ const getWeather = (lat, lon) => {
 			const temp = res.data.main.temp
 			const hum = res.data.main.humidity
 			const status = Object.assign({}, ...res.data.weather)
-			// console.log(res);
+			const id = status.id.toString()
+			
+			// Checking the code using RegExp
+			conditions(id)
 
 			temperature.textContent = temp.toFixed(1) + '°C'
 			humidity.textContent = hum + '%'
@@ -47,21 +51,54 @@ const getWeather = (lat, lon) => {
 		})
 		.catch(err => {
 			console.error(err)
+			warning.textContent = 'Wpisz poprawną nazwę miasta!'
 		})
-}
-
-const keyCheck = (e) => {
-	if (e.key === 'Enter') {
-		getGeoCode()
-	} else if (e.key === 'Escape') {
-		clearInput()	
 	}
-}
+	
+	const keyCheck = e => {
+		if (e.key === 'Enter') {
+			getGeoCode()
+		} else if (e.key === 'Escape') {
+			clearInput()
+		}
+	}
+	
+	const clearInput = () => {
+		input.value = ''
+		warning.textContent = ''
+	}
+	
+	const conditions = e => {
+	// RegExp attempt
+	// e = '800' // testing value
+	const thunderstorm = /^2[0-9]{2}/
+	const drizzle = /^3[0-9]{2}/
+	const rain = /^5[0-9]{2}/
+	const snow = /^6[0-9]{2}/
+	const atmosphere = /^7[0-9]{2}/
+	const clear = /^800/
+	const clouds = /^80[0-9]?/
 
-const clearInput = () => {
-	input.value = ''
+	if (e.match(thunderstorm)) {
+		photo.setAttribute('src', './img/thunderstorm.png')
+	} else if (e.match(drizzle)) {
+		photo.setAttribute('src', './img/drizzle.png')
+	} else if (e.match(rain)) {
+		photo.setAttribute('src', './img/rain.png')
+	} else if (e.match(snow)) {
+		photo.setAttribute('src', './img/ice.png')
+	} else if (e.match(atmosphere)) {
+		photo.setAttribute('src', './img/fog.png')
+	} else if (e.match(clear)) {
+		photo.setAttribute('src', './img/sun.png')
+	} else if (e.match(clouds)) {
+		photo.setAttribute('src', './img/cloud.png')
+	} else {
+		photo.setAttribute('src', './img/unknown.png')
+	}
 }
 
 button.addEventListener('click', getGeoCode)
 input.addEventListener('keyup', keyCheck)
 input.addEventListener('click', clearInput)
+window.addEventListener('DOMContentLoaded', getGeoCode)
