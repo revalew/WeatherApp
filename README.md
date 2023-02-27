d0cc6070a57aa06b0ba56feff41d290f6b724c14
revalew <115182776+revalew@users.noreply.github.com>
2023-02-27 21:19:21 +0100

    created initial structure of the app

---
+++ README.md
@@ -0,0 +1,11 @@
+# [Weather App - JavaScript app](https://revalew.github.io/WeatherApp/)
+
+This is the app created using JavaScript as a part of the website developement course.
+
+What I want to achieve:
+- I want to learn how to create websites, write functional apps in JavaScript and how to use Git & GitHub,
+- This repository was created to practice working in a shared environment.
+
+##
+#### <sup>The course can be accessed [here](https://www.udemy.com/course/javascript-jedyny-kurs-ktorego-potrzebujesz/) (course conducted in polish).
+</sup>
---
+++ js/main.js
@@ -0,0 +1,12 @@
+// Strict mode aplies some restrictions so we could not make stupid things (which are obviously allowed in JS by default), e.g., creating variables without specifying their type, i.e., const / let
+'use strict'
+// -------------------------------------
+
+const input = document.querySelector('input')
+const button = document.querySelector('button')
+const cityName = document.querySelector('.city-name')
+const warning = document.querySelector('.warning')
+const photo = document.querySelector('.photo')
+const weather = document.querySelector('.weather')
+const temperature = document.querySelector('.temperature')
+const humidity = document.querySelector('.humidity')
